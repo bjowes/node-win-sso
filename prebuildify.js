8cda1d1cc76a77cdea74271682a54ed8211dd8c3
Björn Weström <bjowes@gmail.com>
2019-09-30 23:47:01 +0300

    renamed prebuild to avoid duplicate build

--- prebuildify.js
+++ prebuildify.js
@@ -3,10 +3,10 @@ const spawn = require('cross-spawn');
 
 if (os.platform() === 'win32') {
   console.log('Windows OS detected. Prebuilding native modules')
-  spawn.sync('npm', ['run', 'native_prebuild'], {
+  spawn.sync('npm', ['run', 'native_prebuildify'], {
     input: 'Windows OS detected. Prebuilding native module.',
     stdio: 'inherit'
   });
 } else {
-  console.log('Unsupported OS detected. Native module requires Windows OS.');
+  console.log('Unsupported OS detected. Native module requires Windows OS. Aborting prebuildify.');
 }
